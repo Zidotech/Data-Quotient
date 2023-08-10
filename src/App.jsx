2a9c1e480ec971d6f34cc17640b1fd2e00508f3b
Zulu <adekunmbi18@gmail.com>
2023-08-10 11:48:22 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <>
       <p className=" text-center text-yellow-500">
         Click on the Vite and React logos to learn more
+        <span>rebeca</span>
       </p>
     </>
   );
