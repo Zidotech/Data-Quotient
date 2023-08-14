6157ac525b35e06fa5cf2e4417a253347f694fe7
Zulu <adekunmbi18@gmail.com>
2023-08-14 11:54:23 +0100

    added new component

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,11 @@
 import logo from "../assets/logo.png";
+import end from "../assets/icon-end-to-end-3.svg";
+import team from "../assets/icon-team-extension-3.svg";
+import consult from "../assets/icon-consulting-expertise-3.svg";
 
 export default {
   logo,
+  end,
+  team,
+  consult,
 };
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -24,3 +24,30 @@ export const links = [
     text: "Resources",
   },
 ];
+
+export const serviceCard = [
+  {
+    id: "1",
+    title: "End-to-End Development",
+    icon: image.end,
+    description:
+      " 'We deliver world-class projects from initial discovery all the way to deployment and maintenance.' ",
+    stack: " LEARN MORE ",
+  },
+  {
+    id: "2",
+    title: "Team Extension",
+    icon: image.team,
+    description:
+      " 'If you’re short on tech specialists, missing deadlines, or going over budget—we’re here for you.' ",
+    stack: " LEARN MORE ",
+  },
+  {
+    id: "3",
+    title: "Expert Consulting",
+    icon: image.consult,
+    description:
+      " 'We can use our nearly 20 years of experience to plug into your business and help you grow faster.' ",
+    stack: " LEARN MORE ",
+  },
+];
